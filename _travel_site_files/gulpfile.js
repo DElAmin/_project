062fd60fe731f1392bc2f246eb64775f3ce15cfc
DElAmin <delamin13@gmail.com>
2021-01-19 02:21:13 +0100

    Using Webpack Dev Server

--- _travel_site_files/gulpfile.js
+++ _travel_site_files/gulpfile.js
@@ -1,4 +1,30 @@
 var gulp = require('gulp');
-gulp.task('default', function( {
-    console.log("Yeaah");
-}))
+var watch = require('gulp-watch');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssvars = require('postcss-simple-vars');
+var nested = require('postcss-nested');
+
+gulp.task('default', function() {
+   console.log("Yeaah"); 
+});
+
+gulp.task('html', function() {
+    console.log("This is for HTML");
+});
+
+gulp.task('style', function() {
+    return gulp.src('./app/assets/styles/style.css')
+    .pipe(postcss([cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles'));
+});
+
+gulp.task('watch', function() {
+    watch('./app/index.html', function() {
+        gulp.start('html');
+    });
+
+    watch('./app/assets/styles/**/*.css', function() {
+        gulp.start('style');
+     });
+});
